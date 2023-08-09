f420cefe5d1ede0ec93509735e3831381be4ee1e
Mrsandeeprai <133948972+Mrsandeeprai@users.noreply.github.com>
2023-08-09 12:55:32 +0530

    Update ping.py

--- Mickey/modules/ping.py
+++ Mickey/modules/ping.py
@@ -1,5 +1,5 @@
-# Don't remove This Line From Here. Tg: @rajdausaardxop | @J4jaaanu
-# Github :- NobitaRdX | XhackerRdX
+# Don't remove This Line From Here. Tg: @Santani_hu_bro | @Mr_selfish_person
+# Github :- Devilking12 | Mrsandeeprai
 
 import random
 from datetime import datetime
@@ -30,7 +30,7 @@ async def ping(_, message: Message):
 
     ms = (datetime.now() - start).microseconds / 1000
     await loda.edit_text(
-        text=f"нey вαву!!\n{MickeyBot.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [『♕︎𓆩𝗥𝗗𝗫𓆪𝗥⟁𝗝™♕︎』](https://t.me/rajdausaardxop) ||</b>",
+        text=f"нey вαву!!\n{MickeyBot.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [🇴͢ғͦғᷱɪᷡᴄͤ͢ɪᷢᴀʟ᭄🇰ɪɴɢ≛ ᚜](https://t.me/Santani_hu_bro) ||</b>",
         reply_markup=InlineKeyboardMarkup(PNG_BTN),
     )
     if message.chat.type == ChatType.PRIVATE:
